dc7bf0019d13fa944841983a72838187a5b1fa69
nikhilrajpoot21 <nikhil.rajpoot.2104@gmail.com>
2025-09-02 18:52:27 +0530

    Add all the Routes to the root folders of all the Pages

--- inkflow/src/App.js
+++ inkflow/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login'
 import SignUp from './pages/signUp'
 import Home from './pages/home';
+import Dashboard from './pages/dashboard';
+import Settings from './pages/settings';
+import CreatePost from './pages/createPost';
 import ProtectedRoute from './components/protectedRoute';
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/createPost" element={<CreatePost />} />
       </Routes>
     </Router>
   );
