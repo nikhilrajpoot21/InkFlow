8d8c793c0b7393ff2fd36003e7b97f1dac7b39d3
nikhilrajpoot21 <nikhil.rajpoot.2104@gmail.com>
2025-09-07 14:01:31 +0530

    Remove axios from UI files

--- inkflow/src/pages/createPost.js
+++ inkflow/src/pages/createPost.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import api from "../utils/api";
 
 const CreatePost = () => {
--- inkflow/src/pages/viewPost.js
+++ inkflow/src/pages/viewPost.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams} from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import api from "../utils/api";
 
 export default function ViewPost() {
