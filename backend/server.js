7d03f29b8a026fb5dd4fe4042b371ee237d4b7e8
nikhilrajpoot21 <nikhil.rajpoot.2104@gmail.com>
2025-09-07 12:20:17 +0530

    Fix package.json and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
 require('dotenv').config();
 
-PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
